427b72a6202c760608914547239f8ca104695648
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-03 22:02:31 +0600

    hero section done

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="max-w-6xl mx-auto hero bg-purple-600 text-white pb-56 pt-8 rounded-xl">
+      <div className="hero-content text-center">
+        <div className="max-w-4xl">
+          <h1 className="text-5xl font-bold">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
+          <p className="py-6 text-gray-300">
+          Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
+          </p>
+          <button className="btn rounded-full">Shop Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
