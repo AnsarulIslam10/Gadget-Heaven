f84fd9ce51b06c8507fb0953ae9f6f5fb768a69f
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-04 19:47:52 +0600

    filter category gedgets

--- src/components/Gedgets/Gedgets.jsx
+++ src/components/Gedgets/Gedgets.jsx
@@ -3,11 +3,21 @@ import { useLoaderData, useParams } from 'react-router-dom';
 import GedgetCard from '../GedgetCard/GedgetCard';
 
 const Gedgets = () => {
-    const products = useLoaderData()
+    const [gedgets, setGedgets] = useState([]);
+    const data = useLoaderData()
+    const { category } = useParams();
+    useEffect(()=>{
+        if(category){
+            const categoryGedgets = data.filter((product)=> product.category === category);
+            setGedgets(categoryGedgets); 
+        }else{
+            setGedgets(data);
+        }
+    },[data, category])
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
             {
-                products?.map(product=> <GedgetCard key={product.product_id} product={product}></GedgetCard>)
+                gedgets?.map(product=> <GedgetCard key={product.product_id} product={product}></GedgetCard>)
             }
         </div>
     );
